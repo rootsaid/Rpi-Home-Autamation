01edbc7e06aa0211f5e73c1effc57a7eb1591a1e
Root Said <jithinsanal1610@gmail.com>
2017-11-19 20:42:35 +0530

    Update robot_controller.py

--- robot_controller.py
+++ robot_controller.py
@@ -2,58 +2,34 @@
 #For Complete Tutorial, visit http://rootsaid.com/robot-control-over-wifi/
 
 import RPi.GPIO as GPIO
-import 
-socket
-import 
-csv
+import socket
+import csv
 
 GPIO.setwarnings(False)
-
-
-
 GPIO.setmode(GPIO.BOARD)
-
 GPIO.setup(33,GPIO.OUT)
-
 GPIO.setup(11,GPIO.OUT)
-
-
 GPIO.setup(13,GPIO.OUT)
-
 GPIO.setup(15,GPIO.OUT)
-
-
 GPIO.setup(29,GPIO.OUT)
-
 GPIO.setup(31,GPIO.OUT)
 
-
-
 GPIO.output(29,True)
-
 GPIO.output(31,True)
-
-
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5050
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
-
 sock.bind((UDP_IP, UDP_PORT))
 
-
 while True:
-    
 data, addr = sock.recvfrom(1024)
-    
 raw=data
-    
-print raw
-   
  
+#print raw
+
 if raw=="forward":
- 	
 GPIO.output(33,True)
 GPIO.output(11,False)
 GPIO.output(13,True)
@@ -62,7 +38,6 @@ print "Robot Move Forward"
   
   
 elif raw=="stop":
-
 GPIO.output(33,False)
 GPIO.output(11,False)
 GPIO.output(13,False)
@@ -71,16 +46,13 @@ print "Robot Stop"
     
 
 elif raw=="backward":
-	
 GPIO.output(33,False)
 GPIO.output(11,True)
 GPIO.output(13,False)
 GPIO.output(15,True)
 print "Robot Move Backward"
 
-
 elif raw=="left":
-
 GPIO.output(33,False)
 GPIO.output(11,True)
 GPIO.output(13,True)
